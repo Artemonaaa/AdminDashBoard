c0a5a441a84cf2c1795cdcd68b89dc06a3b78f02
Artemona <artemidaabc@gmail.com>
2023-02-19 22:01:25 +0200

    Added sidebar. Added photo and downoload reactprosidebar

--- src/scenses/global/SideBar.jsx
+++ src/scenses/global/SideBar.jsx
@@ -1,7 +1,94 @@
-import React from 'react'
+import { useState } from 'react'
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar'
+import { Box, IconButton, Typography, useTheme } from "@mui/material"
+import { Link } from 'react-router-dom'
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import { tokens } from '../../theme'
+import 'react-pro-sidebar/dist/css/styles.css'
+import { fontWeight } from '@mui/system';
 
 export default function SideBar() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState('Dashboard')
+
   return (
-    <div>SideBar</div>
+    <Box sx={{
+      "& .pro-sidebar-inner": {
+      background: `${colors.primary[400]} !important`,
+    },
+    "& .pro-icon-wrapper": {
+      backgroundColor: "transparent !important",
+    },
+    "& .pro-inner-item": {
+      padding: "5px 35px 5px 20px !important",
+    },
+    "& .pro-inner-item:hover": {
+      color: "#868dfb !important",
+    },
+    "& .pro-menu-item.active": {
+      color: "#6870fa !important",
+      }
+    }}>
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  ADMINS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+          {!isCollapsed && (
+            <Box mb='25px'>
+              <Box display='flex' justifyContent='center' alignItems='center'>
+                <img 
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  src={`../../assets/user.png`}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                  />
+              </Box>
+
+              <Box textAlign='center'>
+                <Typography variant='h2' color={colors.grey[100]} fontWeight='bold' sx={{m: ' 10px 0 0 0'}}>B. Artem</Typography>
+                <Typography variant='h5' color={colors.greenAccent[500]}>VP Fancy Admin</Typography>
+              </Box>
+            </Box>
+          )}
+        </Menu>
+      </ProSidebar>
+    </Box>
   )
 }
+ 

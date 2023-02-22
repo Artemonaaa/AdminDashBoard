a4ab910c1d6b2f37815acacde9bbcfea5404903e
Artemona <artemidaabc@gmail.com>
2023-02-22 20:23:55 +0200

    Added page Team and Contacts. Fix some file name

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import { ColorModeContext, useMode } from './theme'
 import  TopBar  from './scenses/global/TopBar'
 import  SideBar  from './scenses/global/SideBar'
 import  DashBoard  from './scenses/dashboard/index'
-//import  Team  from './scenses/team'
+import  Team  from './scenses/team'
 //import  Invoices  from './scenses/invoices'
-//import  Contacts  from './scenses/contacts'
+import  Contacts  from './scenses/contacts'
 //import  Bar  from './scenses/bar'
 //import  Form  from './scenses/form'
 //import  Line  from './scenses/line'
 //import  Pie  from './scenses/pie'
 //import  FAQ  from './scenses/faq'
 //import  Geography from './scenses/geography'
-//import  Calendar from './scenses/сalendar'
+//import  Calendar from './scenses/calendar'
 
 
 export default function App() {
@@ -29,18 +29,18 @@ export default function App() {
             <TopBar />
             <Routes>
               <Route path='/' element={<DashBoard />}/>
-              {/*
               <Route path='/team' element={<Team />}/>
-              <Route path='/invoices' element={<Invoices />}/>
               <Route path='/contacts' element={<Contacts />}/>
-              <Route path='/bar' element={<Bar />}/>
-              <Route path='/form' element={<Form />}/>
-              <Route path='/line' element={<Line />}/>
-              <Route path='/pie' element={<Pie />}/>
-              <Route path='/faq' element={<FAQ />}/>
-              <Route path='/geography' element={<Geography />}/>
-              <Route path='/сalendar' element={<Calendar />}/>
-              */}
+              {
+              //<Route path='/invoices' element={<Invoices />}/>
+              //<Route path='/bar' element={<Bar />}/>
+              //<Route path='/form' element={<Form />}/>
+              //<Route path='/line' element={<Line />}/>
+              //<Route path='/pie' element={<Pie />}/>
+              //<Route path='/faq' element={<FAQ />}/>
+              //<Route path='/geography' element={<Geography />}/>
+              //<Route path='/сalendar' element={<Calendar />}/>
+              }
             </Routes>
           </main>
         </div>
